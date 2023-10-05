49281f2a6c11876eadd96b0178078c10ceb3191a
Michal Sedivka <sedivka9108@gmail.com>
2023-10-05 10:22:02 +0200

    test

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/App";
 import "@/index.css";
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import { setupListeners } from "@reduxjs/toolkit/query";
+// import { Provider } from "react-redux";
+// import { configureStore } from "@reduxjs/toolkit";
+// import { setupListeners } from "@reduxjs/toolkit/query";
 import { api } from "@/state/api";
 
 export const store = configureStore({
