9c41ad6b0e475b9c46e87cae0b6e9b7c15f224f0
michal9108 <sedivka9108@gmail.com>
2024-03-12 14:23:43 +0100

    commit2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,9 @@ import { Box } from "@mui/material";
 import { CssBaseline } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "@/scenes/dashboard";
-import Login from "./scenes/auth/Login";
-import Signup from "./scenes/auth/Signup";
-import Logout from "./scenes/auth/Logout";
+// import Login from "./scenes/auth/Login";
+// import Signup from "./scenes/auth/Signup";
+// import Logout from "./scenes/auth/Logout";
 import RedirectIfAuthenticated from "@/middleware/RedirectIfAuthenticated";
 import RequireAuth from "@/middleware/RequireAuth";
 
@@ -15,14 +15,12 @@ function App() {
         <CssBaseline />
         <Box width="100%" height="100%">
           <Routes>
-            <Route
-              path="/"
-              element={<RedirectIfAuthenticated Component={Login} />}
-            />
+            {/* <Route  path="/" element={<RedirectIfAuthenticated Component={Login} />}
+            /> */}
             <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/login" element={<Login />} />
+            {/* <Route path="/login" element={<Login />} />
             <Route path="/logout" element={<Logout />} />
-            <Route path="/signup" element={<Signup />} />
+            <Route path="/signup" element={<Signup />} /> */}
             <Route path="/predictions" element={<div>Predictions Page</div>} />
           </Routes>
         </Box>
