b4e661c02f60d8764e8070729d65ab614892a266
michal9108 <sedivka9108@gmail.com>
2024-03-12 16:35:03 +0100

    auth fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,9 +4,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "@/scenes/dashboard";
 import Login from "./scenes/auth/Login";
 import Signup from "./scenes/auth/Signup";
-// import Logout from "./scenes/auth/Logout";
-// import RedirectIfAuthenticated from "@/middleware/RedirectIfAuthenticated";
-// import RequireAuth from "@/middleware/RequireAuth";
+import Logout from "./scenes/auth/Logout";
+import RedirectIfAuthenticated from "@/middleware/RedirectIfAuthenticated";
+import RequireAuth from "@/middleware/RequireAuth";
 
 function App() {
   return (
@@ -15,11 +15,16 @@ function App() {
         <CssBaseline />
         <Box width="100%" height="100%">
           <Routes>
-            {/* <Route  path="/" element={<RedirectIfAuthenticated Component={Login} />}
-            /> */}
-            <Route path="/" element={<Dashboard />} />
+            <Route
+              path="/"
+              element={<RedirectIfAuthenticated Component={Login} />}
+            />
+            <Route
+              path="/dashboard"
+              element={<RequireAuth Component={Dashboard} />}
+            />
             <Route path="/login" element={<Login />} />
-            {/* <Route path="/logout" element={<Logout />} /> */}
+            <Route path="/logout" element={<Logout />} />
             <Route path="/signup" element={<Signup />} /> *
             <Route path="/predictions" element={<div>Predictions Page</div>} />
           </Routes>
--- client/src/scenes/auth/Logout.tsx
+++ client/src/scenes/auth/Logout.tsx
@@ -1,57 +1,57 @@
-// import React, { useState } from "react";
-// import axios from "axios";
-// import {
-//   Button,
-//   Box,
-//   Typography,
-//   Menu,
-//   MenuItem,
-//   useMediaQuery,
-// } from "@mui/material";
-// import { Menu as MenuIcon, ArrowDropDownOutlined } from "@mui/icons-material";
-// import FE_API_URL from "@/config";
-// type Props = {};
+import React, { useState } from "react";
+import axios from "axios";
+import {
+  Button,
+  Box,
+  Typography,
+  Menu,
+  MenuItem,
+  useMediaQuery,
+} from "@mui/material";
+import { Menu as MenuIcon, ArrowDropDownOutlined } from "@mui/icons-material";
+import FE_API_URL from "@/config";
+type Props = {};
 
-// const Logout = () => {
-//   const [anchorEl, setAnchorEl] = useState(null);
-//   const isOpen = Boolean(anchorEl);
-//   const handleClose = () => setAnchorEl(null);
-//   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => setAnchorEl(event.currentTarget);
+const Logout = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isOpen = Boolean(anchorEl);
+  const handleClose = () => setAnchorEl(null);
+  const handleClick = (event: any) => setAnchorEl(event.currentTarget);
 
-//   const handleLogout = async () => {
-//     const token = localStorage.getItem("token");
+  const handleLogout = async () => {
+    const token = localStorage.getItem("token");
 
-//     // Call the server-side logout route
-//     await axios.post(`${FE_API_URL}/logout`, { token });
+    // Call the server-side logout route
+    await axios.post('https://server-dashboard-mern.fly.dev/logout', { token });
 
-//     // Clear the token from local storage
-//     localStorage.removeItem("token");
+    // Clear the token from local storage
+    localStorage.removeItem("token");
 
-//     // Redirect or perform other logout-related actions
-//     // For example, you can redirect to the login page
-//     window.location.href = "/login";
-//   };
+    // Redirect or perform other logout-related actions
+    // For example, you can redirect to the login page
+    window.location.href = "/login";
+  };
 
-//   return (
-//     <>
-//       <Button onClick={handleClick}>
-//         <Box textAlign="left">
-//           <Typography fontWeight="bold" fontSize="1rem" sx={{ color: "white" }}>
-//             User
-//           </Typography>
-//         </Box>
-//         <ArrowDropDownOutlined sx={{ color: "white", fontSize: "25px" }} />
-//       </Button>
-//       <Menu
-//         anchorEl={anchorEl}
-//         open={isOpen}
-//         onClose={handleClose}
-//         anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-//       >
-//         <MenuItem onClick={handleLogout}>Log Out</MenuItem>
-//       </Menu>
-//     </>
-//   );
-// };
+  return (
+    <>
+      <Button onClick={handleClick}>
+        <Box textAlign="left">
+          <Typography fontWeight="bold" fontSize="1rem" sx={{ color: "white" }}>
+            User
+          </Typography>
+        </Box>
+        <ArrowDropDownOutlined sx={{ color: "white", fontSize: "25px" }} />
+      </Button>
+      <Menu
+        anchorEl={anchorEl}
+        open={isOpen}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+      >
+        <MenuItem onClick={handleLogout}>Log Out</MenuItem>
+      </Menu>
+    </>
+  );
+};
 
-// export default Logout;
+export default Logout;
